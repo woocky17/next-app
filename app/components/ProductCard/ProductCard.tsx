8fe15511132c2a5a74cd3d64c5cbf6863da08513
David Ortet <davidortetsabater@gmail.com>
2024-12-17 12:28:31 +0100

    Tailwind CSS

--- app/components/ProductCard/ProductCard.tsx
+++ app/components/ProductCard/ProductCard.tsx
@@ -1,11 +1,10 @@
 //* "use client"; Esto combierte el componente en un comp cliente de default es un comp server
 import React from "react";
 import AddToCard from "../AddToCard";
-import styles from "./ProductCard.module.css";
 
 const ProductCard = () => {
   return (
-    <div className={styles.card}>
+    <div className="p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-900">
       <AddToCard />
     </div>
   );
