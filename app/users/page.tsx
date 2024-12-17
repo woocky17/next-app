efd14676b1545624c5ec67622325fee542410cdf
David Ortet <davidortetsabater@gmail.com>
2024-12-17 12:51:54 +0100

    Routing Overview

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,36 +1,11 @@
 import React from "react";
+import UserTable from "./UserTable";
 
-interface User {
-  id: number;
-  name: string;
-  email: string;
-}
-
-const UsersPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-    cache: "no-store" /* next: { revalidate: 10 }*/,
-  });
-  const users: User[] = await res.json();
+const UsersPage = () => {
   return (
     <>
       <h1>Users</h1>
-      {/* <p>{new Date().toLocaleTimeString()}</p> */}
-      <table className="table table-zebra-zebra">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <UserTable />
     </>
   );
 };
