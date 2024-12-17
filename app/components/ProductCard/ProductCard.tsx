d337c644e2b0cdf35d870a0138ffcb7aa08c03f9
David Ortet <davidortetsabater@gmail.com>
2024-12-17 12:24:28 +0100

    CSS Modules

--- app/components/ProductCard/ProductCard.tsx
+++ app/components/ProductCard/ProductCard.tsx
@@ -1,10 +1,11 @@
 //* "use client"; Esto combierte el componente en un comp cliente de default es un comp server
 import React from "react";
-import AddToCard from "./AddToCard";
+import AddToCard from "../AddToCard";
+import styles from "./ProductCard.module.css";
 
 const ProductCard = () => {
   return (
-    <div>
+    <div className={styles.card}>
       <AddToCard />
     </div>
   );
