8760de2a555ea8ff45279c5b9af5de59d3ea5c65
David Ortet <davidortetsabater@gmail.com>
2024-12-18 09:53:39 +0100

    Programmatic Navigation

--- app/users/[[...sortOrder]]/page.tsx
+++ app/users/[[...sortOrder]]/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import UserTable from "./UserTable";
+import Link from "next/link";
 
 interface Props {
   searchParams: Promise<{ sortOrder: string }>;
@@ -11,6 +12,9 @@ const UsersPage = async ({ searchParams }: Props) => {
   return (
     <>
       <h1>Users </h1>
+      <Link href={"/users/new"} className="btn">
+        New User
+      </Link>
       <UserTable sortOrder={sortOrder} />
     </>
   );
--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,7 +1,14 @@
+"use client";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const NewUsersPage = () => {
-  return <div>NewUsersPage</div>;
+  const router = useRouter();
+  return (
+    <button onClick={() => router.push("/users")} className="btn btn-primary">
+      Create
+    </button>
+  );
 };
 
 export default NewUsersPage;
