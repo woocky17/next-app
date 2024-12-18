6ac4e66341a4920845e3f1e42c561c124f6d8e2a
David Ortet <davidortetsabater@gmail.com>
2024-12-18 11:18:13 +0100

    Updating an Object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -13,3 +13,19 @@ export async function GET(
     { id: 2, name: "jose" },
   ]);
 }
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: number }> }
+) {
+  const body = await request.json();
+
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+
+  const id = (await params).id;
+  if (id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  return NextResponse.json({ id: 1, name: body.name });
+}
