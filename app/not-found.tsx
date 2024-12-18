9969a235ac38b2cc9a9781aee815503e3e03e6ee
David Ortet <davidortetsabater@gmail.com>
2024-12-18 10:15:53 +0100

    Handiling Unexpected Errors

---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return <div>The request page dosent exist</div>;
+};
+
+export default NotFoundPage;
---
+++ app/users/[id]/not-found.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserNotFoundPage = () => {
+  return <div>This user dosen't exist.</div>;
+};
+
+export default UserNotFoundPage;
