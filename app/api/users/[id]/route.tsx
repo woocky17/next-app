2203e8366b185c795d00ac4dab1e249245fec884
David Ortet <davidortetsabater@gmail.com>
2024-12-18 13:58:38 +0100

    Updating Data

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -19,18 +19,26 @@ export async function GET(
 
 export async function PUT(
   request: NextRequest,
-  { params }: { params: Promise<{ id: number }> }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   const body = await request.json();
   const validation = schema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
 
-  const id = (await params).id;
-  if (id > 10)
+  const id = parseInt((await params).id);
+  const user = await prisma.user.findUnique({
+    where: { id: id },
+  });
+  if (!user)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
 
-  return NextResponse.json({ id: 1, name: body.name });
+  const updatedUser = await prisma.user.update({
+    where: { id: user.id },
+    data: { name: body.name, email: body.email },
+  });
+
+  return NextResponse.json(updatedUser);
 }
 
 export async function DELETE(
