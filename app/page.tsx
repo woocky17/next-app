99a1024f59a278b3b9ea6517fbb9ac7805efa028
David Ortet <davidortetsabater@gmail.com>
2024-12-20 11:47:32 +0100

    lodash sorter

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,24 @@
+"use client";
+
+import { Metadata } from "next";
 import Image from "next/image";
+import dynamic from "next/dynamic";
+import { useState } from "react";
+
+import _ from "lodash";
+
+// const HeavyComponent = dynamic(() => import("./components/HeavyComponent"), {
+//   ssr: false,
+//   loading: () => (
+//     <>
+//       <br />
+//       <p className="loading loading-dots loading-sm"></p>
+//     </>
+//   ),
+// });
 
 export default function Home() {
+  // const [isVisible, setVisible] = useState(false);
   return (
     <main className="relative h-screen">
       {/* <Image
@@ -15,6 +33,16 @@ export default function Home() {
         priority
       /> */}
       <h1>Hello World</h1>
+      <button
+        onClick={() => {
+          const users = [{ name: "c" }, { name: "b" }, { name: "a" }];
+          const sorted = _.orderBy(users, ["name"]);
+          console.log(sorted);
+        }}
+      >
+        Show
+      </button>
+      {/* {isVisible && <HeavyComponent />} */}
     </main>
   );
 }
