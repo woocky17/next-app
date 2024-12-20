c00bedb697cacac1633d92ef205f0b0b6eff22ab
David Ortet <davidortetsabater@gmail.com>
2024-12-20 10:30:03 +0100

    Optimizing Images

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
-import Link from "next/link";
-import ProductCard from "./components/ProductCard/ProductCard";
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+import Image from "next/image";
 
-export default async function Home() {
-  const session = await getServerSession(authOptions);
+export default function Home() {
   return (
-    <main>
-      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
-      <Link href={"/users"}>Users</Link>
-      <ProductCard />
+    <main className="relative h-screen">
+      <Image
+        src="https://bit.ly/49LJwnM"
+        alt="vaque"
+        // width={400}
+        // height={100}
+        fill
+        className="object-cover"
+        sizes="(max-width: 480px) 80vw, (max-width: 768px) 50vw, 33vw"
+        quality={100}
+        priority
+      />
     </main>
   );
 }
